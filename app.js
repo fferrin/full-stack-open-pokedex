0b5f009d624bd9e82562d5aad2062362610fc3d5
Facundo Ferrín <facundo.ferrin@gmail.com>
2021-01-07 04:03:09 +0100

    Add rollback and simulate error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('fail')
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
@@ -18,3 +18,5 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+throw new Error()
